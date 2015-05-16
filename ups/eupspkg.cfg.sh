f926b3d354dbc6d431fdee1f22bb9848ad0dcc15
Mario Juric <mjuric@lsst.org>
2015-05-16 01:14:24 -0700

    eupspkg.cfg.sh: append to $CONFIG_OPTIONS, don't override

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,6 +1,6 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix=${PREFIX} --with-apr=${APR_DIR} --with-apr-util=${APR_UTIL_DIR}"
+CONFIGURE_OPTIONS="${CONFIGURE_OPTIONS} --with-apr=${APR_DIR} --with-apr-util=${APR_UTIL_DIR}"
 
 #build()
 #{
